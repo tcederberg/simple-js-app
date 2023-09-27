5a83105bab352ec14b0613340dadb28c7d3cdab7
tcederberg <145338286+tcederberg@users.noreply.github.com>
2023-09-27 07:29:15 -0400

    If applied, this commit will add pokemonList variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-var favoriteFood = 'Pizza';
-document.write(favoriteFood);
-
+let pokemonList = [
+  {name: 'Squirtle', height: 50, types: ['Water'], abilities: ['Rain dish', 'Torrent']},
+  {name: 'Ninetales', height: 110, types: ['Fire'], abilities: ['Flash fire', 'Drought']},
+  {name: 'Alakazam', height: 150, types: ['Psychic'], abilities: ['Synchronize', 'Inner focus']}
+];
